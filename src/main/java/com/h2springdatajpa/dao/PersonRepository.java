cc8af438bd5b76995a611897c45412580bfee91e
chouknat <hicham.chouknat@gmail.com>
2017-02-23 15:25:40 +0100

    Update PersonRepository.java

--- src/main/java/com/h2springdatajpa/dao/PersonRepository.java
+++ src/main/java/com/h2springdatajpa/dao/PersonRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by choukTutos on 18/12/2016.
  */
-public interface PersonRepository extends JpaRepository<Person, Integer> {
+public interface PersonRepository extends PagingAndSortingRepository<Person, Integer> {
 }
